81a7871f45852c780ac825841890eb1738e449ec
edaucher <maumaumausi@hotmail.de>
2014-10-11 21:49:53 +0200

    Konstruktor, Methoden überschreiben
    Konstruktor ergänzt (minqual, minrep)
    
    Methoden equals und hashCode überschrieben (für JUnit Tests benötigt)

--- Kunde.java
+++ Kunde.java
@@ -108,5 +108,41 @@ abstract class Kunde {
 		}
 
 	}
+	
+	
+	
+	@Override
+	public int hashCode() {		// Überschreiben für JUnit benötigt
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + minqual;
+		result = prime * result + minrep;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override					// Überschreiben für JUnit benötigt
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kunde other = (Kunde) obj;
+		if (id != other.id)
+			return false;
+		if (minqual != other.minqual)
+			return false;
+		if (minrep != other.minrep)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 }
