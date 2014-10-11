81702c75f30a6fc4b43d05ef57ebb174d9ab2456
edaucher <maumaumausi@hotmail.de>
2014-10-11 21:08:09 +0200

    this.name aus Konstruktor entfernt

--- KundeHigh.java
+++ KundeHigh.java
@@ -2,7 +2,6 @@ class KundeHigh extends Kunde {
 
 	public KundeHigh(int id, String name, int minrep, int minqual) {
 		super(id, name, minrep, minqual);
-		this.name = name;
 	}
 
 	public void erzAuftrag() {
