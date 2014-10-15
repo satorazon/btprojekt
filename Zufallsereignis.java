98d7268c3b5e39901eb24e0731ae35051f6b61ed
edaucher <maumaumausi@hotmail.de>
2014-10-16 01:25:11 +0200

    nur 2 Zahlen für Test eingefügt

--- Zufallsereignis.java
+++ Zufallsereignis.java
@@ -12,8 +12,8 @@ public class Zufallsereignis {
 			switch (ran) {
 			case 1:
 				ereignis = "Ein Kontrolleur wird sich zum Ende des Quartals eines Ihrer Werke ansehen."
-						+ "Verbessern Sie Ihre Arbeitsbedingungen für 5.000 (1) oder nehmen Sie eine schlechte Bewertung in kauf -20 Rep(2)? "
-						+ "Sie können auch versuchen die Situation mit einem Schmiergeld abzuwenden.(3)";
+						+ "Verbessern Sie Ihre Arbeitsbedingungen fÃ¼r 5.000 (1) oder nehmen Sie eine schlechte Bewertung in kauf -20 Rep(2)? "
+						+ "Sie kÃ¶nnen auch versuchen die Situation mit einem Schmiergeld abzuwenden.(3)";
 				geld = 5000;
 				rep = 20;
 				break;
@@ -32,8 +32,8 @@ public class Zufallsereignis {
 
 			case 4:
 				ereignis = "blabla";
-				geld = 0;
-				rep = 0;
+				geld = 8000;
+				rep = 30;
 				break;
 
 			case 5:
@@ -47,8 +47,8 @@ public class Zufallsereignis {
 			switch (ran) {
 			case 1:
 				ereignis = "Ein Kontrolleur wird sich zum Ende des Quartals eines Ihrer Werke ansehen."
-						+ "Verbessern Sie Ihre Arbeitsbedingungen für 5.000 (1) oder nehmen Sie eine schlechte Bewertung in kauf -20 Rep(2)? "
-						+ "Sie können auch versuchen die Situation mit einem Schmiergeld abzuwenden.(3)";
+						+ "Verbessern Sie Ihre Arbeitsbedingungen fÃ¼r 5.000 (1) oder nehmen Sie eine schlechte Bewertung in kauf -20 Rep(2)? "
+						+ "Sie kÃ¶nnen auch versuchen die Situation mit einem Schmiergeld abzuwenden.(3)";
 				geld = 5000;
 				rep = 20;
 				break;
